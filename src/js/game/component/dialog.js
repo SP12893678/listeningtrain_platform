98c17601f8e0fa6da460130c1b4a105bbec3fb30
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 04:45:31 +0800

    Update dialog.js

--- src/js/game/component/dialog.js
+++ src/js/game/component/dialog.js
@@ -13,20 +13,25 @@ let Application = PIXI.Application,
 export default class Dialog extends Overlay {
     constructor(){
         super();
-        this.settingDialog();
+        this.settingDialog(Config.screen.width,Config.screen.height);
     }
-    settingDialog(){
+    settingDialog(width,height){
         let dialog = new PIXI.Graphics();
-        this.dialogWidth = Config.screen.width/4;
-        this.dialogHeight = Config.screen.height/3;
+        this.dialogWidth = width/4;
+        this.dialogHeight = height/3;
         dialog.beginFill(0x000000,0.8);
         dialog.drawRoundedRect(0,0,this.dialogWidth,this.dialogHeight,10);
         dialog.endFill();
         dialog.position.set((this.width-this.dialogWidth)/2,(this.height-this.dialogHeight)/2);
         this.addChild(dialog);
         this.dialog = dialog;
-        this.button = new Button3(100,50,'hello');
-        this.addChild(this.button.container);
+
+        this.cancelBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'取消');
+        this.cancelBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2,this.dialog.y+this.dialogHeight*0.7);
+        this.yesBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'確認');
+        this.yesBtn.position.set(this.cancelBtn.x+this.cancelBtn.width+this.dialogWidth*0.05,this.cancelBtn.y);
+        this.addChild(this.cancelBtn);
+        this.addChild(this.yesBtn);
     }
 
     setDialog(width,height){
