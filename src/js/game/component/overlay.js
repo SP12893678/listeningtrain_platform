ab8eca998e77cb802387e6877eacaceb0067698f
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 03:42:56 +0800

    change the size of overlay
    using the parameters
    Config.screen.width
    Config.screen.height

--- src/js/game/component/overlay.js
+++ src/js/game/component/overlay.js
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js'
-import ScenesManager from '@/js/game/engine/ScenesManager'
+import Config from '@/js/game/Config'
 
 export default class Overlay extends PIXI.Container {
     constructor() {
@@ -11,7 +11,10 @@ export default class Overlay extends PIXI.Container {
     setOverlay() {
         var overlay = new PIXI.Graphics()
         overlay.beginFill(0x000000, 0.45)
-        overlay.drawRect(0, 0, ScenesManager.defaultWidth, ScenesManager.defaultHeight)
+        overlay.drawRect(0, 0, Config.screen.width, Config.screen.height)
+        console.log('width',Config.screen.width);
+        console.log('height',Config.screen.height);
+
         overlay.endFill()
         this.addChild(overlay)
         this.overlay = overlay
