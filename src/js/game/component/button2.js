294119a70a5875eccf7e00d1bc1464dcb34f50a2
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 04:13:37 +0800

    Update button2.js

--- src/js/game/component/button2.js
+++ src/js/game/component/button2.js
@@ -10,33 +10,33 @@ let Application = PIXI.Application,
 /**
  * A button component 
  * @constructor
- * @param {number} width - The width of the button
- * @param {number} height - The height of the button
- * @param {number} border - The border of the button
+ * @param {number} btnWidth - The btnWidth of the button
+ * @param {number} btnHeight - The btnHeight of the button
+ * @param {number} btnBorder - The btnBorder of the button
  * @param {number} cornerRadius - The corner radius of the button
- * @param {number} backgroundColor - The background color of the button
+ * @param {number} btnBgColor - The background color of the button
  * @param {string} iconPath - The resource of the icon 
- * @param {string} label - The label(Text) of the button
+ * @param {string} btnLabel - The btnLabel(Text) of the button
  */
-export default class Button2{
-    constructor(width, height,iconPath,label) {
-        this.width = width;
-        this.height = height;
-        this.border = 4;
-        this.borderColor = "0xFFFFFF";
+export default class Button2 extends PIXI.Container{
+    constructor(btnWidth, btnHeight,iconPath,btnLabel) {
+        super();
+        this.btnWidth = btnWidth;
+        this.btnHeight = btnHeight;
+        this.btnBorder = 4;
+        this.btnBorderColor = "0xFFFFFF";
         this.cornerRadius = 30;
-        this.backgroundColor = "0x000000";
+        this.btnBgColor = "0x000000";
         this.iconPath = iconPath;
-        this.label = label;
-        this.container = new PIXI.Container();
+        this.btnLabel = btnLabel;
         this.setButton();
         this.setText();
-        this.container.interactive = true;
-        this.container.buttonMode = true;
-        this.container.mouseover = function(mouseData) {
+        this.interactive = true;
+        this.buttonMode = true;
+        this.mouseover = function(mouseData) {
             this.alpha = 0.95;
         }
-        this.container.mouseout = function(mouseData) {
+        this.mouseout = function(mouseData) {
             this.alpha = 1;
         }
     }
@@ -44,28 +44,27 @@ export default class Button2{
     setButton(){
        /* Draws a button */
        this.button = new PIXI.Graphics();
-       this.button.lineStyle(this.border,this.borderColor);
-       this.button.beginFill(this.backgroundColor);//填充
-       this.button.drawRoundedRect(0,0,this.width,this.height,this.cornerRadius);
+       this.button.lineStyle(this.btnBorder,this.btnBorderColor);
+       this.button.beginFill(this.btnBgColor);//填充
+       this.button.drawRoundedRect(0,0,this.btnWidth,this.btnHeight,this.cornerRadius);
        this.button.endFill();
        var icon = new Sprite (PIXI.loader.resources [this.iconPath].texture);
-    //    var icon = new PIXI.Sprite.fromImage("../"+this.iconPath);
-       icon.width = this.height*0.7;;
-       icon.height = this.height*0.7;
-       icon.position.set(this.width*0.1,this.height*0.3/2);
+       icon.width = this.btnHeight*0.7;;
+       icon.height = this.btnHeight*0.7;
+       icon.position.set(this.btnWidth*0.1,this.btnHeight*0.3/2);
        this.button.addChild(icon);
-       this.container.addChild(this.button);
+       this.addChild(this.button);
     }
-    setBorder(border){
-        this.border = border;
+    setBorder(btnBorder){
+        this.btnBorder = btnBorder;
         this.reDraw();
     }
-    setBorderColor(borderColor){
-        this.borderColor = borderColor;
+    setBorderColor(btnBorderColor){
+        this.btnBorderColor = btnBorderColor;
         this.reDraw();
     }
-    setBackgroundColor(backgroundColor){
-        this.backgroundColor = backgroundColor;
+    setBackgroundColor(btnBgColor){
+        this.btnBgColor = btnBgColor;
         this.reDraw();
     }
     setCornerRadius(cornerRadius){
@@ -74,20 +73,20 @@ export default class Button2{
     }
     reDraw(){
         this.button.clear();
-        this.button.lineStyle(this.border,this.borderColor);
-        this.button.beginFill(this.backgroundColor);//填充
-        this.button.drawRoundedRect(0,0,this.width,this.height,this.cornerRadius);
+        this.button.lineStyle(this.btnBorder,this.btnBorderColor);
+        this.button.beginFill(this.btnBgColor);//填充
+        this.button.drawRoundedRect(0,0,this.btnWidth,this.btnHeight,this.cornerRadius);
         this.button.endFill();
     }
     setText(){
-        var text = new PIXI.Text(this.label, {
+        var text = new PIXI.Text(this.btnLabel, {
             fontFamily: 'Noto Sans TC',
-            fontSize: this.height*0.5,
+            fontSize: this.btnHeight*0.5,
             fill: 0xf1f1f1,
             align: 'center',
             fontWeight: '400',
         })
-        text.position.set((this.height*0.8+this.width - text.width) / 2, (this.height - text.height) / 2);
-        this.container.addChild(text);
+        text.position.set((this.btnHeight*0.8+this.btnWidth - text.width) / 2, (this.btnHeight - text.height) / 2);
+        this.addChild(text);
     }
 }
