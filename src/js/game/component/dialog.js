99ae9fd07f18a88f5a3df0286fedb9665919d0d4
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 19:48:19 +0800

    Update dialog.js
    add label to the dialog
    add setBackgroundColor(color,alpha) to change the background color of dialog
    add draw() to draw a dialog graphic
    set yesBtn & cancelBtn
    add action of dialog when clicking

--- src/js/game/component/dialog.js
+++ src/js/game/component/dialog.js
@@ -11,30 +11,67 @@ let Application = PIXI.Application,
     Sprite = PIXI.Sprite
 
 export default class Dialog extends Overlay {
-    constructor(){
+    constructor(label){
         super();
+        this.dialogLabel = label;
         this.settingDialog(Config.screen.width,Config.screen.height);
+        this.click = () => {
+            if(this.visible)
+                this.visible = false;
+            else 
+                this.visible = true;
+        }
     }
     settingDialog(width,height){
-        let dialog = new PIXI.Graphics();
+        this.dialog = new PIXI.Graphics();
+        this.addChild(this.dialog);
         this.dialogWidth = width/4;
         this.dialogHeight = height/3;
-        dialog.beginFill(0x000000,0.8);
-        dialog.drawRoundedRect(0,0,this.dialogWidth,this.dialogHeight,10);
-        dialog.endFill();
-        dialog.position.set((this.width-this.dialogWidth)/2,(this.height-this.dialogHeight)/2);
-        this.addChild(dialog);
-        this.dialog = dialog;
-
+        this.dialogBgColor = 0x000000;
+        this.dialogBgColorAlpha = 0.8;
+        this.draw();
+        this.setLabel();
+        this.setButton();
+    }
+    setButton(){
         this.cancelBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'取消');
-        this.cancelBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2,this.dialog.y+this.dialogHeight*0.7);
+        this.cancelBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2,this.dialog.y+this.dialogHeight*0.6);
+        // this.cancelBtn.setBackgroundColor(0xffff00,0.6);
+        // this.cancelBtn.setTextColor(0x000000);
         this.yesBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'確認');
         this.yesBtn.position.set(this.cancelBtn.x+this.cancelBtn.width+this.dialogWidth*0.05,this.cancelBtn.y);
+        this.yesBtn.setBackgroundColor(0xffd700,0.95);
+        this.yesBtn.setTextColor(0x000000);
         this.addChild(this.cancelBtn);
         this.addChild(this.yesBtn);
     }
-
-    setDialog(width,height){
+    setLabel(){
+        let text = new PIXI.Text(this.dialogLabel, {
+            fontFamily: 'Noto Sans TC',
+            fontSize: 36,
+            fill: 0xf1f1f1,
+            align: 'center',
+            fontWeight: '400',
+        })
+        text.position.set(this.dialog.x+(this.dialogWidth-36*this.dialogLabel.length)/2 ,this.dialog.y+this.dialogHeight*0.3);
+        this.text = text;
+        this.addChild(this.text);
+    }
+    setLabelColor(color){
+        this.text.tint = color;
+    }
+    setSize(width,height){
         
+    }
+    setBackgroundColor(color,alpha){
+        this.dialogBgColor = color;
+        this.dialogBgColorAlpha = alpha;
+        this.draw();
+    }
+    draw(){
+        this.dialog.beginFill(this.dialogBgColor,this.dialogBgColorAlpha);
+        this.dialog.drawRoundedRect(0,0,this.dialogWidth,this.dialogHeight,10);
+        this.dialog.endFill();
+        this.dialog.position.set((this.width-this.dialogWidth)/2,(this.height-this.dialogHeight)/2);
     }
 }
