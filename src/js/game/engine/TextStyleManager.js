6d1b00ce49ee72883e3517550b4c9c74b2a0675f
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 21:55:47 +0800

    Update TextStyleManager.js
    change "defult" to "default"

--- src/js/game/engine/TextStyleManager.js
+++ src/js/game/engine/TextStyleManager.js
@@ -5,10 +5,10 @@ let TextStyle = PIXI.TextStyle
  * Custom font was added by game.css
  */
 
-const defult = new TextStyle({
+const default = new TextStyle({
     fontFamily: 'jf-openhuninn',
     fontSize: 24,
     fill: '#000000',
 })
 
-export { defult }
+export { default }
