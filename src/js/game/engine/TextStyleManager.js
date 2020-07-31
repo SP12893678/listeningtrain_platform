64990a8b0d78f8ba6a98eeae2d52cb2b3aea8e20
Emily Luo <acs106104@gm.ntcu.edu.tw>
2020-07-31 22:07:48 +0800

    Update TextStyleManager.js
    add style2

--- src/js/game/engine/TextStyleManager.js
+++ src/js/game/engine/TextStyleManager.js
@@ -10,5 +10,17 @@ const style1 = new TextStyle({
     fontSize: 24,
     fill: '#000000',
 })
+const style2 = new TextStyle({
+    fontFamily: 'Arial',
+    fontSize: 36,
+    fill: ' white ',
+    stroke: '#0x66FF33',
+    strokeThickness: 4,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+})
 
-export { style1 }
+export { style1,style2 }
