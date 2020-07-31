68738eb8c7ea7e1a8e0ebd62f5d3f5fb8e8bdc3e
SP12893678 <office1289366@gmail.com>
2020-07-31 18:27:12 +0800

    test pull confict

--- src/js/game/component/dialog.js
+++ src/js/game/component/dialog.js
@@ -11,30 +11,28 @@ let Application = PIXI.Application,
     Sprite = PIXI.Sprite
 
 export default class Dialog extends Overlay {
-    constructor(){
-        super();
-        this.settingDialog(Config.screen.width,Config.screen.height);
+    constructor() {
+        super()
+        this.settingDialog(Config.screen.width, Config.screen.height)
     }
-    settingDialog(width,height){
-        let dialog = new PIXI.Graphics();
-        this.dialogWidth = width/4;
-        this.dialogHeight = height/3;
-        dialog.beginFill(0x000000,0.8);
-        dialog.drawRoundedRect(0,0,this.dialogWidth,this.dialogHeight,10);
-        dialog.endFill();
-        dialog.position.set((this.width-this.dialogWidth)/2,(this.height-this.dialogHeight)/2);
-        this.addChild(dialog);
-        this.dialog = dialog;
+    settingDialog(width, height) {
+        let dialog = new PIXI.Graphics()
+        this.dialogWidth = width / 4
+        this.dialogHeight = height / 3
+        dialog.beginFill(0x000000, 0.8)
+        dialog.drawRoundedRect(0, 0, this.dialogWidth, this.dialogHeight, 10)
+        dialog.endFill()
+        dialog.position.set((this.width - this.dialogWidth) / 2, (this.height - this.dialogHeight) / 2)
+        this.addChild(dialog)
+        this.dialog = dialog
 
-        this.cancelBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'取消');
-        this.cancelBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2,this.dialog.y+this.dialogHeight*0.7);
-        this.yesBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'確認');
-        this.yesBtn.position.set(this.cancelBtn.x+this.cancelBtn.width+this.dialogWidth*0.05,this.cancelBtn.y);
-        this.addChild(this.cancelBtn);
-        this.addChild(this.yesBtn);
+        this.cancelBtn = new Button3((this.dialogWidth * 0.75) / 2, this.dialogHeight * 0.2, '取消')
+        this.cancelBtn.position.set(this.dialog.x + (this.dialogWidth - this.dialogWidth * 0.8) / 2, this.dialog.y + this.dialogHeight * 0.7)
+        this.yesBtn = new Button3((this.dialogWidth * 0.75) / 2, this.dialogHeight * 0.2, '確認')
+        this.yesBtn.position.set(this.cancelBtn.x + this.cancelBtn.width + this.dialogWidth * 0.05, this.cancelBtn.y)
+        this.addChild(this.cancelBtn)
+        this.addChild(this.yesBtn)
     }
 
-    setDialog(width,height){
-        
-    }
+    setDialog(width, height) {}
 }
