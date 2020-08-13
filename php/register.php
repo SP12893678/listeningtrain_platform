76a0179ff4a0c7ef53c0a39576c064a6107889a1
unknown <wenya1024@gmail.com>
2020-08-13 21:06:29 +0800

    there is a new sql in sql

--- php/register.php
+++ php/register.php
@@ -5,16 +5,17 @@
     $acr = $_GET['acr'];
     $pwr = $_GET['pwr'];
     $id = $_GET['id'];
+
   
-    $sql = "SELECT * FROM login WHERE  account='$acr'";
+    $sql = "SELECT * FROM user WHERE account='$acr'";
     $account_repeat =mysqli_query($con, $sql);
     $total_account = mysqli_num_rows($account_repeat);
     if ( $total_account == 0 ) {
-        $sql2 = "SELECT * FROM login WHERE  username='$name'";
+        $sql2 = "SELECT * FROM user WHERE name='$name'";
         $name_repeat =mysqli_query($con, $sql2);
         $total_name = mysqli_num_rows($name_repeat);
         if ( $total_name == 0 ) {
-        $register = "INSERT INTO login (username, mail,account,password,id) VALUES ('$name', '$mail','$acr','$pwr','$id')";
+        $register = "INSERT INTO user (name, mail,account,password,identity) VALUES ('$name', '$mail','$acr','$pwr','$id')";
         mysqli_query($con,$register);
         echo 1;
         }else{ // name repeat
