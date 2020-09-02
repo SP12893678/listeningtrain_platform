0b87e200c89df35e902d7491a070ef96d8d48c00
SP12893678 <office1289366@gmail.com>
2020-09-02 23:25:24 +0800

    enviro manage check complete dialog

--- php/files.php
+++ php/files.php
@@ -1,11 +1,9 @@
 <?php
-    $type = $_GET['type']
+    $type = $_GET['type'];
     switch ($type) {
         case 'upload':
             
             break;
-        
         default:
-            # code...
             break;
     }
