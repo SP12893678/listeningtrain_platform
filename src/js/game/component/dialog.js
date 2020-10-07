5d91267a727e87844d5687b8dbced424a305bce2
unknown <wenya1024@gmail.com>
2020-10-08 00:08:58 +0800

    10/8 video and guide

--- src/js/game/component/dialog.js
+++ src/js/game/component/dialog.js
@@ -11,14 +11,14 @@ let Application = PIXI.Application,
     Sprite = PIXI.Sprite
 
 export default class Dialog extends Overlay {
-    constructor(label='',type = 0){
+    constructor(label = '', type = 0) {
         super();
         this.dialogLabel = label;
         this.type = type
-        
+
         this.dialog = new PIXI.Graphics();
-        this.dialogWidth = Config.screen.width/4;
-        this.dialogHeight = Config.screen.height/3;
+        this.dialogWidth = Config.screen.width / 4;
+        this.dialogHeight = Config.screen.height / 3;
         this.dialogBgColor = 0x000000;
         this.dialogBgColorAlpha = 0.8;
         this.settingDialog();
@@ -26,10 +26,10 @@ export default class Dialog extends Overlay {
         //         this.visible = false;
         // }
     }
-    settingDialog(){
+    settingDialog() {
         this.addChild(this.dialog);
         this.draw();
-        switch(this.type){
+        switch (this.type) {
             case 0:
                 this.setLabel();
                 this.setYesButton();
@@ -42,6 +42,9 @@ export default class Dialog extends Overlay {
                 this.setLabel();
                 this.setYesButton();
                 break;
+            case 3:
+
+                break;
             default:
                 this.setLabel();
                 this.setYesButton();
@@ -51,31 +54,31 @@ export default class Dialog extends Overlay {
         // this.setLabel();
         // this.setButton();
     }
-    setYesButton(){
-        let yesBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'確認');
-        yesBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2+yesBtn.btnWidth+this.dialogWidth*0.05,this.dialog.y+this.dialogHeight*0.6);
-        if(this.type == 2){
-            yesBtn.pivot.set(yesBtn.btnWidth/2,yesBtn.btnHeight/2)
-            yesBtn.position.set(this.dialog.x+this.dialogWidth/2,this.dialog.y+this.dialogHeight*0.7);
+    setYesButton() {
+        let yesBtn = new Button3(this.dialogWidth * 0.75 / 2, this.dialogHeight * 0.20, '確認');
+        yesBtn.position.set(this.dialog.x + (this.dialogWidth - this.dialogWidth * 0.8) / 2 + yesBtn.btnWidth + this.dialogWidth * 0.05, this.dialog.y + this.dialogHeight * 0.6);
+        if (this.type == 2) {
+            yesBtn.pivot.set(yesBtn.btnWidth / 2, yesBtn.btnHeight / 2)
+            yesBtn.position.set(this.dialog.x + this.dialogWidth / 2, this.dialog.y + this.dialogHeight * 0.7);
         }
-        yesBtn.setCornerRadius(this.dialogHeight*0.20/(2.1));
-        yesBtn.setBackgroundColor(0xffd700,0.95);
+        yesBtn.setCornerRadius(this.dialogHeight * 0.20 / (2.1));
+        yesBtn.setBackgroundColor(0xffd700, 0.95);
         yesBtn.setTextColor(0x000000);
         this.addChild(yesBtn);
 
         this.yesBtn = yesBtn
     }
-    setCancelButton(){
-        let cancelBtn = new Button3(this.dialogWidth*0.75/2,this.dialogHeight*0.20,'取消');
-        cancelBtn.position.set(this.dialog.x+(this.dialogWidth-this.dialogWidth*0.8)/2,this.dialog.y+this.dialogHeight*0.6);
-        cancelBtn.setCornerRadius(this.dialogHeight*0.20/(2.1));
+    setCancelButton() {
+        let cancelBtn = new Button3(this.dialogWidth * 0.75 / 2, this.dialogHeight * 0.20, '取消');
+        cancelBtn.position.set(this.dialog.x + (this.dialogWidth - this.dialogWidth * 0.8) / 2, this.dialog.y + this.dialogHeight * 0.6);
+        cancelBtn.setCornerRadius(this.dialogHeight * 0.20 / (2.1));
         // this.cancelBtn.setBackgroundColor(0xffff00,0.6);
         // this.cancelBtn.setTextColor(0x000000);
         this.addChild(cancelBtn);
 
         this.cancelBtn = cancelBtn
     }
-    setLabel(){
+    setLabel() {
         let text = new PIXI.Text(this.dialogLabel, {
             fontFamily: 'Noto Sans TC',
             fontSize: 36,
@@ -84,14 +87,14 @@ export default class Dialog extends Overlay {
             fontWeight: '400',
         })
         text.anchor.set(0.5)
-        text.position.set(this.dialog.x+this.dialogWidth/2,this.dialog.y+this.dialogHeight*0.4);
+        text.position.set(this.dialog.x + this.dialogWidth / 2, this.dialog.y + this.dialogHeight * 0.4);
         this.text = text;
         this.addChild(this.text);
     }
-    setLabelColor(color){
+    setLabelColor(color) {
         this.text.tint = color;
     }
-    setCloseButton(){
+    setCloseButton() {
         this.closeBtnBgColor = 0xffffff;
         this.closeBtnBgColorAlpha = 0.95;
         this.cloaseBtnRadius = 15;
@@ -103,11 +106,11 @@ export default class Dialog extends Overlay {
             this.visible = false;
         }
 
-        this.closeBtn.beginFill(this.closeBtnBgColor,this.closeBtnColorAlpha);
-        this.closeBtn.drawCircle(this.dialog.x+this.dialog.width,this.dialog.y,this.cloaseBtnRadius*2);
+        this.closeBtn.beginFill(this.closeBtnBgColor, this.closeBtnColorAlpha);
+        this.closeBtn.drawCircle(this.dialog.x + this.dialog.width, this.dialog.y, this.cloaseBtnRadius * 2);
         this.closeBtn.endFill();
         this.closeBtn.lineStyle(2, 0xffffff, 1);
-        this.closeBtn.drawCircle(this.dialog.x+this.dialog.width,this.dialog.y,this.cloaseBtnRadius*1.7);
+        this.closeBtn.drawCircle(this.dialog.x + this.dialog.width, this.dialog.y, this.cloaseBtnRadius * 1.7);
 
         let text = new PIXI.Text('✕', {
             fontFamily: 'Noto Sans TC',
@@ -117,42 +120,42 @@ export default class Dialog extends Overlay {
             fontWeight: '400',
         })
         text.anchor.set(0.5);
-        text.position.set(this.dialog.x+this.dialog.width,this.dialog.y);
+        text.position.set(this.dialog.x + this.dialog.width, this.dialog.y);
         this.closeBtnText = text;
         this.closeBtn.addChild(this.closeBtnText);
     }
-    setCloseBtnBackgroundColor(color,alpha){
+    setCloseBtnBackgroundColor(color, alpha) {
         this.closeBtnBgColor = color;
         this.closeBtnBgColorAlpha = alpha;
         this.draw();
     }
-    setSize(width,height){
+    setSize(width, height) {
         this.removeChildren()
         this.addChild(this.overlay)
         this.dialogWidth = width
         this.dialogHeight = height
         this.settingDialog()
     }
-    setBackgroundColor(color,alpha){
+    setBackgroundColor(color, alpha) {
         this.dialogBgColor = color;
         this.dialogBgColorAlpha = alpha;
         this.draw();
     }
-    draw(){
+    draw() {
         this.dialog.clear()
-        this.dialog.beginFill(this.dialogBgColor,this.dialogBgColorAlpha);
-        this.dialog.drawRoundedRect(0,0,this.dialogWidth,this.dialogHeight,10);
+        this.dialog.beginFill(this.dialogBgColor, this.dialogBgColorAlpha);
+        this.dialog.drawRoundedRect(0, 0, this.dialogWidth, this.dialogHeight, 10);
         this.dialog.endFill();
-        this.dialog.position.set((this.width-this.dialogWidth)/2,(this.height-this.dialogHeight)/2);
-        if(this.closeBtn != null){
+        this.dialog.position.set((this.width - this.dialogWidth) / 2, (this.height - this.dialogHeight) / 2);
+        if (this.closeBtn != null) {
             this.closeBtn.clear();
-            this.closeBtn.beginFill(this.closeBtnBgColor,this.closeBtnColorAlpha);
+            this.closeBtn.beginFill(this.closeBtnBgColor, this.closeBtnColorAlpha);
             this.closeBtn
-            this.closeBtn.drawCircle(this.dialog.x+this.dialog.width,this.dialog.y,this.cloaseBtnRadius*2);
+            this.closeBtn.drawCircle(this.dialog.x + this.dialog.width, this.dialog.y, this.cloaseBtnRadius * 2);
             this.closeBtn.endFill();
             this.closeBtn.lineStyle(2, 0xffffff, 1);
-            this.closeBtn.drawCircle(this.dialog.x+this.dialog.width,this.dialog.y,this.cloaseBtnRadius*1.7);
-            this.closeBtnText.position.set(this.dialog.x+this.dialog.width,this.dialog.y);
+            this.closeBtn.drawCircle(this.dialog.x + this.dialog.width, this.dialog.y, this.cloaseBtnRadius * 1.7);
+            this.closeBtnText.position.set(this.dialog.x + this.dialog.width, this.dialog.y);
         }
     }
 }
--- src/js/game/engine/ResourcesManager.js
+++ src/js/game/engine/ResourcesManager.js
@@ -43,6 +43,7 @@ import goBack from '@/assets/images/icon/goBack.png'
 import help from '@/assets/images/icon/help.png'
 import clock from '@/assets/images/icon/clock.png'
 import start from '@/assets/images/icon/start.png'
+import pause from '@/assets/images/icon/pause.png'
 import nextQuestion from '@/assets/images/icon/nextQuestion.png'
 import reload from '@/assets/images/icon/reload.png'
 import leave from '@/assets/images/icon/leave.png'
@@ -65,7 +66,7 @@ import circle_go001 from '@/assets/images/circle_go001.png'
 import gear_inside from '@/assets/images/gear-inside.png'
 import gear_center from '@/assets/images/gear-center.png'
 import gear_outside from '@/assets/images/gear-outside.png'
-// import ptvideo from '@/assets/video/practicemode.mp4'
+// import practicevideo from '@/assets/video/practicemode.mp4'
 
 const resources = {
     loading_bg: loadingBackground,
@@ -109,6 +110,7 @@ const resources = {
     help: help,
     clock: clock,
     start: start,
+    pause: pause,
     nextQuestion: nextQuestion,
     reload: reload,
     leave: leave,
@@ -130,7 +132,7 @@ const resources = {
     correctAnswer: correctAnswer,
     yourAnswer: yourAnswer,
     check: check,
-    // ptvideo: ptvideo,
+    // practicevideo: practicevideo,
 }
 
 export default resources
